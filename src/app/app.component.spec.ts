6974ec57f865cf6ac6f6e7e8f08897e727986891
lancedouglas1 <lance.douglas@outlook.com>
2020-03-28 12:33:13 -0600

    cleanup 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8-seed'`, () => {
+  it(`should have as title 'epicognition-org'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular8-seed');
+    expect(app.title).toEqual('epicognition-org');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular8-seed!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to epicognition-org!');
   });
 });
